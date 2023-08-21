e32f2d003acbbb9b35062e861a2c096b464ba3eb
diwakar4586 <diwakar0470.be21@chitkara.eud.in>
2023-08-22 00:09:36 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        deepBlue:"#5c6bc0",
+        
+    
+      },
+    },
+  },
+  plugins: [],
+}
+
